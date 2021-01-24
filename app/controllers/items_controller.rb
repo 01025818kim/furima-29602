337678a059815c3adde0a05c7f312093be0b9c24
01025818kim <hime.09.22kim@icloud.com>
2021-01-24 16:23:11 +0900

    商品一覧機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index ]
 
   def index
+    @items = Item.all
   end
 
   def new
@@ -15,6 +16,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+
+  def show
+  end
+  
   end
 
   private
